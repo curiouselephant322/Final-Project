4b294ddf212bf0cb18a1b5a1a446dca2a484851f
ztav <zaul_tavangar+cs1300@brown.edu>
2022-12-16 11:23:41 -0500

    Updates

--- src/atoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-
-export const animationState = atom({
-  key: 'animationState', // unique ID (with respect to other atoms/selectors)
-  default: 1, // default value (aka initial value)
-});
